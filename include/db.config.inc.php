f6a05be4a1b74767f95a7f0c14431d8228a309df
Dmitry Savin <dsavin@stream.ru>
2016-02-16 17:36:08 +0300

    dbconfig

---
+++ include/db.config.inc.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Конфигурационные данные системы
+ *
+ * @author Craft-Soft Team
+ * @version 1.0 beta
+ * @copyright (C)2013 Craft-Soft.ru.  Все права защищены.
+ * @package CS:Bans
+ * @link http://craft-soft.ru/
+*/
+
+$config->db_host	= 'localhost';
+$config->db_user	= 'gozm';
+$config->db_pass	= '123';
+$config->db_db	= 'gozm_gozm';
+$config->db_prefix	= 'amx';
