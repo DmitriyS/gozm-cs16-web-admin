82844d45d6dd0139d53d07c4dcd4c6cf7aa92d4c
Dmitry Savin <dsavin@stream.ru>
2016-04-25 19:17:59 +0300

    add several favicon sizes

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -32,26 +32,32 @@
     <link href="<?php echo Yii::app()->theme->baseUrl; ?>/css/style-responsive.css" rel="stylesheet">
 
     <!-- favicons -->
-	<link rel="apple-touch-icon apple-touch-icon-precomposed" sizes="57x57" href="/images/favicons/apple-touch-icon-57x57.png" />
-	<link rel="apple-touch-icon apple-touch-icon-precomposed" sizes="60x60" href="/images/favicons/apple-touch-icon-60x60.png" />
-	<link rel="apple-touch-icon apple-touch-icon-precomposed" sizes="72x72" href="/images/favicons/apple-touch-icon-72x72.png" />
-	<link rel="apple-touch-icon apple-touch-icon-precomposed" sizes="76x76" href="/images/favicons/apple-touch-icon-76x76.png" />
-	<link rel="apple-touch-icon apple-touch-icon-precomposed" sizes="114x114" href="/images/favicons/apple-touch-icon-114x114.png" />
-	<link rel="apple-touch-icon apple-touch-icon-precomposed" sizes="120x120" href="/images/favicons/apple-touch-icon-120x120.png" />
-	<link rel="apple-touch-icon apple-touch-icon-precomposed" sizes="144x144" href="/images/favicons/apple-touch-icon-144x144.png" />
-	<link rel="apple-touch-icon apple-touch-icon-precomposed" sizes="152x152" href="/images/favicons/apple-touch-icon-152x152.png" />
+    <meta name="msapplication-config" content="/images/favicons/IEConfig.xml" />
+	<link rel="apple-touch-icon" sizes="57x57" href="/images/favicons/apple-touch-icon-57x57.png" />
+	<link rel="apple-touch-icon" sizes="60x60" href="/images/favicons/apple-touch-icon-60x60.png" />
+	<link rel="apple-touch-icon" sizes="72x72" href="/images/favicons/apple-touch-icon-72x72.png" />
+	<link rel="apple-touch-icon" sizes="76x76" href="/images/favicons/apple-touch-icon-76x76.png" />
+	<link rel="apple-touch-icon" sizes="114x114" href="/images/favicons/apple-touch-icon-114x114.png" />
+	<link rel="apple-touch-icon" sizes="120x120" href="/images/favicons/apple-touch-icon-120x120.png" />
+	<link rel="apple-touch-icon" sizes="144x144" href="/images/favicons/apple-touch-icon-144x144.png" />
+	<link rel="apple-touch-icon" sizes="152x152" href="/images/favicons/apple-touch-icon-152x152.png" />
+	<link rel="apple-touch-icon" sizes="180x180" href="/images/favicons/apple-touch-icon-180x180.png" />
 	<link rel="icon" type="image/png" href="/images/favicons/favicon-16x16.png" sizes="16x16" />
 	<link rel="icon" type="image/png" href="/images/favicons/favicon-32x32.png" sizes="32x32" />
 	<link rel="icon" type="image/png" href="/images/favicons/favicon-96x96.png" sizes="96x96" />
 	<link rel="icon" type="image/png" href="/images/favicons/favicon-128x128.png" sizes="128x128" />
+	<link rel="icon" type="image/png" href="/images/favicons/android-chrome-192x192.png" sizes="192x192" />
 	<link rel="icon" type="image/png" href="/images/favicons/favicon-196x196.png" sizes="196x196" />
-	<meta name="application-name" content="GoZm"/>
-	<meta name="msapplication-TileColor" content="#FFFFFF" />
+	<link rel="manifest" href="/images/favicons/manifest.json">
+	<link rel="mask-icon" href="/images/favicons/safari-pinned-tab.svg" color="#5bbad5">
+	<meta name="application-name" content="GoZombie Web"/>
+	<meta name="msapplication-TileColor" content="#da532c" />
 	<meta name="msapplication-square70x70logo" content="/images/favicons/mstile-70x70.png" />
 	<meta name="msapplication-TileImage" content="/images/favicons/mstile-144x144.png" />
 	<meta name="msapplication-square150x150logo" content="/images/favicons/mstile-150x150.png" />
 	<meta name="msapplication-square310x310logo" content="/images/favicons/mstile-310x310.png" />
 	<meta name="msapplication-wide310x150logo" content="/images/favicons/mstile-310x150.png" />
+	<meta name="theme-color" content="#ffffff">
 
     <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
     <!--[if lt IE 9]>
